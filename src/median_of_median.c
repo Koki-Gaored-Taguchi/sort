76e987de19bde7becd65b578a2d90408d6e5c1e7
KokiTaguchi <taguchi.k.ac@m.titech.ac.jp>
2019-07-17 16:23:13 +0900

    #6

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -14,23 +14,64 @@ void swap(int *p, int *q){
   *q = tmp;
 }
 
+int quick_select_o(int A[], int n, int k){
+    int i, j, pivot;
+    
+    // 真ん中の要素をピボットとする
+    pivot = A[n/2];
+    A[n/2] = A[0];
+    A[0] = pivot;
+    for(i = j = 1; i < n; i++){
+        if(A[i] <= pivot){
+            swap(A+i, A+j);
+            j++;
+        }
+    }
+    
+    if(j == k+1) return pivot;
+    else if(j < k+1) return quick_select_o(A+j, n-j, k-j);
+    else return quick_select_o(A+1, j-1, k);
+}
+
 int quick_select(int A[], int n, int k){
   int i, j, pivot;
+    if(n==1 && k==1){return A[0];}
 
-// 真ん中の要素をピボットとする
-  pivot = A[n/2];
-  A[n/2] = A[0];
-  A[0] = pivot;
-  for(i = j = 1; i < n; i++){
+    for(i=0;i<=((n-1)/5);i++){
+        if(5*i+5<=n){
+ 
+
+            B[i]=quick_select_o(A+5*i,5,3);
+            printf("%d\n",B[i]);
+                    }
+        else{
+            B[i]=quick_select_o(A+5*i,n-5*i,((n-5*i)/2)+1);
+        }
+        
+    }
+    
+    printf("%d\n",n);
+    int m=quick_select(B,((n-1)/5)+1,(((n/5)+1)/2)+1);
+    pivot=m;
+    int h=0;//pivotがA[]に現れたら、その後は常に1で、1の時は要素を一つずつずらしている。
+    for(i=0;i<n;i++){//A[]からpivotを抜く
+        if(A[i]==pivot){h=1;}
+        if(h==1){
+            if(i==n-1){A[i]=0;}
+            else{A[i]=A[i+1];}
+            
+        }
+    }
+  for(i = j = 0; i < n-1; i++){//pivotを抜いたので要素がn-1となった
     if(A[i] <= pivot){
       swap(A+i, A+j);
       j++;
     }
   }
 
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+  if(j == k) return pivot;
+  else if(j < k) return quick_select(A+j, n-1-j, k-j);
+  else return quick_select(A, j, k+1);
 }
 
 
@@ -45,18 +86,5 @@ int main(){
     if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
 //    printf("%d th element is %d\n", i, quick_select(A, N, i));
   }
-    for(i=0;i<=(N/5)+1;i++){
-        if(5*i+5<=N){
-            B[i]=quick_select(A+5*i,5,3);
-            
-        }
-        else{
-            B[i]=quick_select(A+5*i,N-5*i,((N-5*i)/2)+1);
-        }
 
-    }
-
-
-    int m=quick_select(B,(N/5)+1,(((N/5)+1)/2)+1);
-    printf("%d\n",m);
 }
